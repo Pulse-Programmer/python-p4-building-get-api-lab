2f94d448b1b676bc8753726f0c4d4e9960a67425
Stephen Ochieng <materiamystic@gmail.com>
2024-07-04 00:29:49 +0300

    Implements GET Api

--- server/app.py
+++ server/app.py
@@ -20,19 +20,31 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    
+    return make_response(bakeries, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    response_body = bakery.to_dict()
+    
+    return make_response(response_body, 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    sorted_goods_desc = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    
+    response_body = [good.to_dict() for good in sorted_goods_desc]
+    
+    return make_response(response_body, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    response_body = most_expensive.to_dict()
+    
+    return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
